16273438e237861836b2c10a4d53d754b2c0ae67
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-30 14:02:47 +0200

    Render Photo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./styles.css";
-
+import Photo from "./download.jpg";
 export const App = () => {
   return (
     <>
       <h1>React TypeScript Webpack Starter Template</h1>
+      <img src={Photo} alt="" />
     </>
   );
 };
