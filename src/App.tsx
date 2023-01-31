995800ba0cddc64870c826ea8640f3d8d6d3ca99
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-31 09:40:37 +0200

    Change format because of prettier settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import "./styles.css";
-import Photo from "./download.jpg";
-import { Counter } from "./Counter";
+import './styles.css'
+import Photo from './download.jpg'
+import { Counter } from './Counter'
 export const App = () => {
   return (
     <>
       <h1>
-        React TypeScript Webpack Starter Template - {process.env.NODE_ENV}{" "}
+        React TypeScript Webpack Starter Template - {process.env.NODE_ENV}{' '}
         {process.env.name}
       </h1>
       <img src={Photo} alt="" />
       <Counter />
     </>
-  );
-};
+  )
+}
