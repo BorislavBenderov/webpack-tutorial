eaa276ff823b7292a96d566d9ac4f90e0d387f73
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-31 08:50:40 +0200

    Render name

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Photo from "./download.jpg";
 export const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template</h1>
+      <h1>React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
       <img src={Photo} alt="" />
     </>
   );
