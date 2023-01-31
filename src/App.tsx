3a9befc22e47e37e8e638465a515641a6913e519
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-31 09:00:12 +0200

    Render Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./styles.css";
 import Photo from "./download.jpg";
+import { Counter } from "./Counter";
 export const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
+      <h1>
+        React TypeScript Webpack Starter Template - {process.env.NODE_ENV}{" "}
+        {process.env.name}
+      </h1>
       <img src={Photo} alt="" />
+      <Counter />
     </>
   );
 };
